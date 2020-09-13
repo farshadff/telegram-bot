b35b3b4330655052164f36db4d1accae8cbaf89c
farshad <farshad.badiee@gmail.com>
2020-09-13 15:59:18 +0430

    `adding main project file`

--- src/TelegramBot.php
+++ src/TelegramBot.php
@@ -20,21 +20,21 @@ class TelegramBot
 
     public function sendMessage($message = null)
     {
-        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/sendMessage?text=' . $message.'&chat_id='.$this->chat_id, []);
+        $this->client->get($this->apiBaseUri.$this->token.'/sendMessage?text='.$message.'&chat_id='.$this->chat_id, []);
     }
 
     public function sendPhoto($photo = null,$caption=null)
     {
-        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/sendPhoto&photo=' . $photo,'&caption='.$caption.'&chat_id='.$this->chat_id, []);
+        $this->client->get($this->apiBaseUri.$this->token.'/sendPhoto&photo='.$photo,'&caption='.$caption.'&chat_id='.$this->chat_id, []);
     }
 
     public function sendLocation($lat = null,$long=null)
     {
-        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/sendLocation&latitude=' . $lat.'&longitude='.$long.'&chat_id='.$this->chat_id, []);
+        $this->client->get($this->apiBaseUri.$this->token.'/sendLocation&latitude='.$lat.'&longitude='.$long.'&chat_id='.$this->chat_id, []);
     }
     public function getChatMembersCount()
     {
-        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/getChatMembersCount&'.'&chat_id='.$this->chat_id, []);
+        $this->client->get($this->apiBaseUri.$this->token.'/getChatMembersCount&'.'&chat_id='.$this->chat_id, []);
     }
 
 }
