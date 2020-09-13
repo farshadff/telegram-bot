3def58fd90dcd19a6fa0a021e0de895c43db01cd
farshad <farshad.badiee@gmail.com>
2020-09-13 14:14:36 +0430

    `adding main project file`

--- src/TelegramBot.php
+++ src/TelegramBot.php
@@ -10,26 +10,26 @@ class TelegramBot
     /**
      * TelegramBot constructor.
      */
-    public function __construct($apiBaseUri=null,$token=null,$chat_id=null)
+    public function __construct()
     {
-        $this->apiBaseUri =config('services.telegram-bot-api.base_uri') ?? 'https://api.telegram.org';
-        $this->token =  config('services.telegram-bot-api.token');
+        $this->apiBaseUri = config('services.telegram-bot-api.base_uri') ?? 'https://api.telegram.org';
+        $this->token = config('services.telegram-bot-api.token');
         $this->chat_id = config('services.telegram-bot-api.chat_id');
+        $this->client = new \GuzzleHttp\Client();
     }
 
-    public function sendMessage($message = null){
-        $client = new \GuzzleHttp\Client();
-        $client->get($this->apiBaseUri.'bot'.$this->token.'sendMessage'.$message, [
-        ]);
+    public function sendMessage($message = null)
+    {
+        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/sendMessage?text=' . $message, []);
     }
-    public function sendPhoto($message = null){
-        $client = new \GuzzleHttp\Client();
-        $client->get($this->apiBaseUri.'bot'.$this->token.'sendMessage'.$message, [
-        ]);
+
+    public function sendPhoto($message = null)
+    {
+        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/sendMessage' . $message, []);
     }
-    public function sendLocation($message = null){
-        $client = new \GuzzleHttp\Client();
-        $client->get($this->apiBaseUri.'bot'.$this->token.'sendMessage'.$message, [
-        ]);
+
+    public function sendLocation($message = null)
+    {
+        $this->client->get($this->apiBaseUri . 'bot' . $this->token . '/sendMessage' . $message, []);
     }
 }
